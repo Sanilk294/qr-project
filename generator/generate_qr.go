06dca95f5c6224b2df084e1a0b506f75d91dbc98
sanil.kadam <sanil.kadma@dhan.co>
2025-12-03 14:45:39 +0530

    added changes for url

--- generator/generate_qr.go
+++ generator/generate_qr.go
@@ -49,8 +49,8 @@ func main() {
 	}
 
 	//siteURL := "http://localhost:10043/"
-	siteURL := "http://localhost:10043/list"
-	// siteURL := "https://YOUR_USERNAME.github.io/qr-project/list"
+	// siteURL := "http://localhost:10043/list"
+	siteURL := "https://Sanilk294.github.io/qr-project/list"
 	outputFile := filepath.Join(outDir, "site_qr.png")
 
 	err = qrcode.WriteFile(siteURL, qrcode.Medium, 512, outputFile)
